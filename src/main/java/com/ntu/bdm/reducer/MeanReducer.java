b2decfbe2993b9faeb0a80e8279c57dad0f17652
lks1226 <56990594+lks1226@users.noreply.github.com>
2022-03-30 23:27:49 +0800

    GH-20 - Add Mean, Median, Standard Deviation

---
+++ src/main/java/com/ntu/bdm/reducer/MeanReducer.java
@@ -0,0 +1,52 @@
+package com.ntu.bdm.reducer;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MeanReducer extends Reducer<Text, FloatWritable, Text, Text> {
+    public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+        float sum = 0F;
+        float sqrSum = 0F;
+        int count = 0;
+        List<Float> list = new ArrayList<>();
+        for (FloatWritable value : values) {
+            float curVal = value.get();
+            sum += curVal;
+            count += 1;
+            list.add(curVal);
+        }
+
+        Collections.sort(list);
+        int len = list.size();
+        float median;
+        if (len % 2 != 0) {
+            median = list.get(len / 2);
+        } else {
+            median = (list.get(len / 2) + list.get(len / 2 - 1)) / 2F;
+        }
+
+        float mean = sum / count;
+
+        for (float f : list){
+            float diff = f - mean;
+            sqrSum = diff * diff;
+        }
+
+        float sd = (float) Math.sqrt(sqrSum / count);
+
+
+        System.out.printf("Key: %s\n", key);
+        System.out.printf("Mean: %s\n", mean);
+        System.out.printf("Median: %s\n", median);
+        System.out.printf("SD: %s\n", sd);
+
+        String output = String.format("%s,%s,%s", mean, median,sd);
+        context.write(key, new Text(output));
+    }
+}
