3e0def326dcdd99657f9d95d317aff387d9c1752
lks1226 <56990594+lks1226@users.noreply.github.com>
2022-03-31 12:25:52 +0800

    GH-21 - Add point runner and comments

--- src/main/java/com/ntu/bdm/reducer/PointReducer.java
+++ src/main/java/com/ntu/bdm/reducer/PointReducer.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public class PointReducer extends Reducer<Text, FloatWritable, Text, KmeanFeature> {
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        // TODO Need to be dynamic = numMonths
         KmeanFeature point = new KmeanFeature(60);
         for (Text value : values) {
             String[] v = value.toString().split("_");
