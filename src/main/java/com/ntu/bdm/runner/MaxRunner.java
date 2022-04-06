0380bb7bbb6469cf1fc501564df59d14967a1e66
lks1226 <56990594+lks1226@users.noreply.github.com>
2022-04-06 18:47:10 +0800

    GH - 33 Change ip address to dynamic (#34)
    * GH-33 - Use ip of machine running jar
    
    * GH-33 - Use ip of master node

--- src/main/java/com/ntu/bdm/runner/MaxRunner.java
+++ src/main/java/com/ntu/bdm/runner/MaxRunner.java
@@ -1,6 +1,7 @@
 package com.ntu.bdm.runner;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 import com.ntu.bdm.mapper.MaxMapper;
 import com.ntu.bdm.reducer.MaxReducer;
@@ -16,8 +17,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class MaxRunner {
     public MaxRunner(String inPath, String outPath) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration conf = new Configuration();
-        conf.set("fs.default.name","hdfs://3.1.36.136:9000");
-        conf.set("yarn.resourcemanager.hostname", "3.1.36.136"); // see step 3
+        String ip = InetAddress.getLocalHost().toString().split("/")[1];
+        conf.set("fs.default.name", String.format("hdfs://%s:9000", ip));
+        conf.set("yarn.resourcemanager.hostname", ip); // see step 3
         conf.set("mapreduce.framework.name", "yarn");
         Job job = Job.getInstance(conf, "Min Max Temp");
 
