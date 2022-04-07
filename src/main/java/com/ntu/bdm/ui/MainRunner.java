f6e5d7763e0d749e19f9d81f44bfb8b8c58adeb9
kaishern <leek0125@e.ntu.edu.sg>
2022-04-08 00:57:40 +0800

    GH-35 - Add user interfaces

---
+++ src/main/java/com/ntu/bdm/ui/MainRunner.java
@@ -0,0 +1,55 @@
+package com.ntu.bdm.ui;
+public class MainRunner {
+    private static Manual askUserChoice() throws Exception {
+        int id = SafeScanner.readInt("Your choice = ");
+        Manual choice = Manual.get(id);
+        if (choice == null) {
+            throw new Exception(String.format("Invalid user choice %d", id));
+        }
+        return choice;
+    }
+
+    public MainRunner() {
+        boolean shouldStop = false;
+        TasksUserInterface ui = new TasksUserInterface();
+        while (!shouldStop) {
+            Manual.printManual();
+            try {
+                Manual userChoice = askUserChoice();
+                switch (userChoice) {
+                    case INIT:
+                        ui.runInitUI();
+                        break;
+                    case MIN_MAX:
+                        ui.runMinMaxUI();
+                        break;
+                    case MEAN_MED_SD:
+                        ui.runMeanMedSDUI();
+                        break;
+                    case POINT:
+                        ui.runPointUI();
+                        break;
+                    case SELECTED:
+                        ui.runSelectedFieldUI();
+                        break;
+                    case KMEAN:
+                        ui.runKmeanUI();
+                        break;
+                    case STOP:
+                        shouldStop = true;
+                        break;
+                    default:
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        SafeScanner.closeReader();
+        System.out.println("Terminating the application...");
+    }
+
+    public static void main(String[] args) {
+        new MainRunner();
+    }
+}
