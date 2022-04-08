0c840c015b8f34b553af57dbb334ced988c38113
kaishern <leek0125@e.ntu.edu.sg>
2022-04-08 10:16:39 +0800

    GH-35 - Change number of criteria to dynamic

--- src/main/java/com/ntu/bdm/reducer/SelectedFieldReducer.java
+++ src/main/java/com/ntu/bdm/reducer/SelectedFieldReducer.java
@@ -7,9 +7,16 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 public class SelectedFieldReducer extends Reducer<Text, Text, Text, Text> {
+    private int numCriterion = 0;
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        this.numCriterion = context.getConfiguration().getInt("numCriterion", 2);
+    }
+
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         // TODO Need to be dynamic = numMonths * numField
-        KmeanFeature point = new KmeanFeature(12 * 2);
+        int totalMonth = 12;
+        KmeanFeature point = new KmeanFeature(totalMonth * numCriterion);
         for (Text value : values) {
             String[] v = value.toString().split("_");
             int idx = Integer.parseInt(v[0]);
@@ -17,8 +24,8 @@ public class SelectedFieldReducer extends Reducer<Text, Text, Text, Text> {
             int ctr = 0;
             for (String s : trimmedVal.split(",")) {
                 float f = Float.parseFloat(s);
-                point.set(idx * 12 + ctr, f);
-                ctr = (ctr + 1) % 12;
+                point.set(idx * totalMonth + ctr, f);
+                ctr = (ctr + 1) % totalMonth;
             }
         }
         context.write(key, new Text(point.toString()));
