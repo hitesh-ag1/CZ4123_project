09d92a390bef3fa9371a7a7c0c824f380f5a220b
lks1226 <56990594+lks1226@users.noreply.github.com>
2022-04-16 14:39:10 +0800

    GH-46 Add windspeed column

--- src/main/java/com/ntu/bdm/mapper/NormMapper.java
+++ src/main/java/com/ntu/bdm/mapper/NormMapper.java
@@ -1,7 +1,6 @@
 package com.ntu.bdm.mapper;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -9,20 +8,22 @@ import java.io.IOException;
 
 
 public class NormMapper extends Mapper<Object, Text, Text, Text> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text wordTmp = new Text();
-    private Text wordHum =  new Text();
     int mindate;
+    private Text wordTmp = new Text();
+    private Text wordHum = new Text();
+    private Text wordWs = new Text();
+
 
     @Override
     public void setup(Context context) throws IOException, InterruptedException {
         Configuration conf = context.getConfiguration();
         this.mindate = conf.getInt("date", 200001);
-        System.out.println("TestMinDate Mapper "+ mindate);
+        System.out.println("TestMinDate Mapper " + mindate);
     }
+
     public void map(Object key, Text value, Context context) throws InterruptedException, IOException {
-        System.out.printf("Key: %s \n",key);
-        System.out.printf("Value: %s \n",value);
+        System.out.printf("Key: %s \n", key);
+        System.out.printf("Value: %s \n", value);
 
         String line = value.toString().split("\\t")[1];
         String[] stringArr = line.split(",");
@@ -30,14 +31,21 @@ public class NormMapper extends Mapper<Object, Text, Text, Text> {
         String yearMonth = stringArr[1];
         if (stringArr[2].equals("M")) stringArr[2] = "NaN";
         if (stringArr[3].equals("M")) stringArr[3] = "NaN";
+        if (stringArr[4].equals("M")) stringArr[4] = "NaN";
+
         float temp = Float.parseFloat(stringArr[2]);
         float humidity = Float.parseFloat(stringArr[3]);
+        float windspeed = Float.parseFloat(stringArr[4]);
 
-        wordTmp.set((station+","+yearMonth));
-        context.write(wordTmp, new Text(String.valueOf(temp)+"T"));
 
-        wordHum.set((station+","+yearMonth));
-        context.write(wordHum, new Text(String.valueOf(humidity)+"H"));
+        wordTmp.set((station + "," + yearMonth));
+        context.write(wordTmp, new Text(String.valueOf(temp) + "T"));
+
+        wordHum.set((station + "," + yearMonth));
+        context.write(wordHum, new Text(String.valueOf(humidity) + "H"));
+
+        wordWs.set((station + "," + yearMonth));
+        context.write(wordWs, new Text(String.valueOf(windspeed) + "W"));
 
     }
 }
